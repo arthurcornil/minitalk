680a7a607e425c4b08d93c128173829bc4907d34
arthurcornil <102752044+arthurcornil@users.noreply.github.com>
2025-05-20 07:49:09 +0200

    reformatted code

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: arcornil <arcornil@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 20:14:14 by arcornil          #+#    #+#             */
-/*   Updated: 2025/05/19 20:40:33 by arcornil         ###   ########.fr       */
+/*   Updated: 2025/05/20 07:48:22 by arcornil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ void	receive_bit(int sig)
 {
 	static t_byte	received_char = (t_byte){0, 0};
 
-	received_char.byte = received_char.byte << 1;
+	received_char.byte <<= 1;
 	if (sig == SIGUSR2)
-		received_char.byte = (received_char.byte | 1);
+		received_char.byte |= 1;
 	received_char.curr_bit ++;
 	if (received_char.curr_bit == sizeof(char) * CHAR_BIT)
 	{
