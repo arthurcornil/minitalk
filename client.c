c6bcd338ce3375d9947909260fddc872e87b6f8e
arthurcornil <102752044+arthurcornil@users.noreply.github.com>
2025-05-20 16:13:59 +0200

    tried implementing a acknowledgment system

--- client.c
+++ client.c
@@ -6,11 +6,20 @@
 /*   By: arcornil <arcornil@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 20:25:28 by arcornil          #+#    #+#             */
-/*   Updated: 2025/05/20 07:45:12 by arcornil         ###   ########.fr       */
+/*   Updated: 2025/05/20 16:13:12 by arcornil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+#include <signal.h>
+
+struct s_ack_status 
+{
+	sig_atomic_t	bit_ack;
+	sig_atomic_t	byte_ack;
+};
+
+volatile struct s_ack_status	g_ack_status;
 
 bool	send_char(pid_t pid, char c)
 {
@@ -22,22 +31,42 @@ bool	send_char(pid_t pid, char c)
 	mask = 1 << (i - 1);
 	while (i > 0)
 	{
+		g_ack_status.bit_ack = false;
 		if (!(mask & c))
 			ret = kill(pid, SIGUSR1);
 		else
 			ret = kill(pid, SIGUSR2);
-		usleep(100);
 		if (ret < 0)
 			return (false);
+		while (!g_ack_status.bit_ack)
+			pause();
 		mask = mask >> 1;
 		i --;
 	}
+	g_ack_status.byte_ack = false;
+	while (!g_ack_status.byte_ack)
+		pause();
 	return (true);
+}
+
+void	handle_ack(int sig)
+{
+	if (sig == SIGUSR1)
+	{
+		ft_putstr_fd(1, "\nGot bit ack\n");
+		g_ack_status.bit_ack = true;
+	}
+	else
+	{
+		ft_putstr_fd(1, "\nGot byte ack\n");
+		g_ack_status.byte_ack = true;
+	}
 }
 
 int	main(int argc, char **argv)
 {
-	pid_t	server_pid;
+	pid_t				server_pid;
+	struct sigaction	sa;
 
 	if (argc != 3)
 	{
@@ -50,8 +79,14 @@ int	main(int argc, char **argv)
 		ft_putstr_fd(2, "Error: Invalid PID.\n");
 		return (1);
 	}
+	sa.sa_handler = handle_ack;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 	while (*argv[2])
-	{
+	{	
+		g_ack_status.bit_ack = false;
 		if (!send_char(server_pid, *argv[2]))
 		{
 			ft_putstr_fd(2, "Error: The message couldn't reach the server in its entirety.\n");
--- server.c
+++ server.c
@@ -6,42 +6,44 @@
 /*   By: arcornil <arcornil@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 20:14:14 by arcornil          #+#    #+#             */
-/*   Updated: 2025/05/20 07:48:22 by arcornil         ###   ########.fr       */
+/*   Updated: 2025/05/20 16:01:01 by arcornil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	receive_bit(int sig)
+void	receive_bit(int sig, siginfo_t *info, void *context)
 {
 	static t_byte	received_char = (t_byte){0, 0};
+	pid_t			client_pid;
 
+	(void)context;
+	client_pid = info->si_pid;
 	received_char.byte <<= 1;
 	if (sig == SIGUSR2)
 		received_char.byte |= 1;
 	received_char.curr_bit ++;
+	kill(client_pid, SIGUSR1);
 	if (received_char.curr_bit == sizeof(char) * CHAR_BIT)
 	{
 		write(1, &received_char.byte, 1);
 		received_char.byte = 0;
 		received_char.curr_bit = 0;
+		kill(client_pid, SIGUSR2);
 	}
 }
 
 int	main(void)
 {
 	pid_t				pid;
-	unsigned char		received_char;
 	struct sigaction	sa;
 
-	(void)received_char;
 	pid = getpid();
-	sa.sa_handler = receive_bit;
+	sa.sa_sigaction = receive_bit;
 	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = 0;
+	sa.sa_flags = SA_SIGINFO;
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
-	received_char = 0;
 	printf("SERVER PID: %d\n", pid);
 	while (true)
 		sleep(1);
