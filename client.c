76cb6beae3a151fcb2a1ef63df87c16cf43af3e9
arthurcornil <102752044+arthurcornil@users.noreply.github.com>
2025-05-20 18:59:40 +0200

    tried to fix unsuccessfully

--- client.c
+++ client.c
@@ -6,20 +6,15 @@
 /*   By: arcornil <arcornil@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 20:25:28 by arcornil          #+#    #+#             */
-/*   Updated: 2025/05/20 16:13:12 by arcornil         ###   ########.fr       */
+/*   Updated: 2025/05/20 18:54:08 by arcornil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 #include <signal.h>
+#include <stdlib.h>
 
-struct s_ack_status 
-{
-	sig_atomic_t	bit_ack;
-	sig_atomic_t	byte_ack;
-};
-
-volatile struct s_ack_status	g_ack_status;
+volatile sig_atomic_t	g_bit_ack;
 
 bool	send_char(pid_t pid, char c)
 {
@@ -31,35 +26,29 @@ bool	send_char(pid_t pid, char c)
 	mask = 1 << (i - 1);
 	while (i > 0)
 	{
-		g_ack_status.bit_ack = false;
+		g_bit_ack = 0;
 		if (!(mask & c))
 			ret = kill(pid, SIGUSR1);
 		else
 			ret = kill(pid, SIGUSR2);
 		if (ret < 0)
 			return (false);
-		while (!g_ack_status.bit_ack)
-			pause();
+		while (!g_bit_ack)
+			usleep(42);
 		mask = mask >> 1;
 		i --;
 	}
-	g_ack_status.byte_ack = false;
-	while (!g_ack_status.byte_ack)
-		pause();
 	return (true);
 }
 
 void	handle_ack(int sig)
 {
 	if (sig == SIGUSR1)
-	{
-		ft_putstr_fd(1, "\nGot bit ack\n");
-		g_ack_status.bit_ack = true;
-	}
+		g_bit_ack = 1;
 	else
 	{
-		ft_putstr_fd(1, "\nGot byte ack\n");
-		g_ack_status.byte_ack = true;
+		ft_putstr_fd(1, "The message was received by the server successfully.");
+		exit(EXIT_SUCCESS);
 	}
 }
 
@@ -86,7 +75,6 @@ int	main(int argc, char **argv)
 	sigaction(SIGUSR2, &sa, NULL);
 	while (*argv[2])
 	{	
-		g_ack_status.bit_ack = false;
 		if (!send_char(server_pid, *argv[2]))
 		{
 			ft_putstr_fd(2, "Error: The message couldn't reach the server in its entirety.\n");
@@ -94,5 +82,12 @@ int	main(int argc, char **argv)
 		}
 		argv[2]++;
 	}
+	if (!send_char(server_pid, '\0'))
+	{
+		ft_putstr_fd(2, "Error: The message couldn't reach the server in its entirety.\n");
+		return (1);
+	}
+	while(1)
+		pause();
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: arcornil <arcornil@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 20:14:14 by arcornil          #+#    #+#             */
-/*   Updated: 2025/05/20 16:01:01 by arcornil         ###   ########.fr       */
+/*   Updated: 2025/05/20 18:57:03 by arcornil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,29 @@
 void	receive_bit(int sig, siginfo_t *info, void *context)
 {
 	static t_byte	received_char = (t_byte){0, 0};
-	pid_t			client_pid;
 
 	(void)context;
-	client_pid = info->si_pid;
+	if (info->si_pid <= 0)
+	{
+		ft_putstr_fd(1, "wtf\n");
+		return ;
+	}
 	received_char.byte <<= 1;
 	if (sig == SIGUSR2)
 		received_char.byte |= 1;
 	received_char.curr_bit ++;
-	kill(client_pid, SIGUSR1);
+	kill(info->si_pid, SIGUSR1);
 	if (received_char.curr_bit == sizeof(char) * CHAR_BIT)
 	{
+		received_char.curr_bit = 0;
+		if (received_char.byte == (char)0)
+		{
+			kill(info->si_pid, SIGUSR2);
+			write(1, "\n", 1);
+			return ;
+		}
 		write(1, &received_char.byte, 1);
 		received_char.byte = 0;
-		received_char.curr_bit = 0;
-		kill(client_pid, SIGUSR2);
 	}
 }
 
@@ -39,13 +47,15 @@ int	main(void)
 	struct sigaction	sa;
 
 	pid = getpid();
-	sa.sa_sigaction = receive_bit;
+	sa.sa_sigaction = &receive_bit;
 	sigemptyset(&sa.sa_mask);
+	sigaddset(&sa.sa_mask, SIGUSR1);
+	sigaddset(&sa.sa_mask, SIGUSR2);
 	sa.sa_flags = SA_SIGINFO;
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
 	printf("SERVER PID: %d\n", pid);
 	while (true)
-		sleep(1);
+		pause();
 	return (0);
 }
